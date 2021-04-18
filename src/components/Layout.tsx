ed02f9982725ea70a45141867c31d275c08c8d58
Carol <hellocharlesp@gmail.com>
2021-04-18 03:07:18 +0200

    Layout fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
-const Layout: React.FC<{ background?: string }> = ({
-  children,
-  background = "trueGray-800",
-}) => {
-  return (
-    <div className={`h-screen w-screen flex bg-${background}`}>{children}</div>
-  );
-};
+const Layout: React.FC = ({ children }) => (
+  <div className="h-screen w-screen flex bg-trueGray-800">{children}</div>
+);
 
 export default Layout;
