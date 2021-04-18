49732c469979aff9bb1b8fc191ef73bda6448dc7
Carol <hellocharlesp@gmail.com>
2021-04-19 00:21:27 +0200

    env fix

--- src/stripejs.ts
+++ src/stripejs.ts
@@ -1,8 +1,9 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-const publishableKey = process.env.STRIPE_PUBLISHABLE_KEY || "";
+const publishableKey = process.env.GATSBY_STRIPE_PUBLISHABLE_KEY;
 
 const getStripe = async () => {
+  if (!publishableKey) throw new Error("No publishable key");
   const stripe = await loadStripe(publishableKey);
   return stripe;
 };
